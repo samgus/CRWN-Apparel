7ced60ca50fad7768e42e93fb5a1235f14016991
Sam Gustafsson <samgus93@gmail.com>
2022-08-18 10:37:26 -0400

    implemented stripe payment method to website

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
-import App from './App';
-import { store, persistor } from './store/store';
+import App from "./App";
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
 );
+
+/*
+TEST CARDS:
+Visa: 4242424242424242
+Visa Debit: 4000056655665556
+Mastercard: 5555555555554444
+Mastercard (2-series): 2223003122003222
+*/
